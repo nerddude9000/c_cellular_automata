e5d50ef0cebf23fa2c8f9e0eff29a81071de5063
nerddude <30808467+nerddude24@users.noreply.github.com>
2025-12-20 23:30:44 +0100

    feat: map init

--- src/constants.h
+++ src/constants.h
@@ -1,8 +1,11 @@
 #ifndef APP_CONSTANTS
 #define APP_CONSTANTS
 
+// square for now
 #define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_HEIGHT 800
+#define MAP_SIZE SCREEN_WIDTH / 10
+#define CELL_SIZE MAP_SIZE / SCREEN_WIDTH
 
 typedef enum { EMPTY, SOLID, FALLING } CellType;
 
--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include "constants.h"
 #include <raylib.h>
 
+void init_map(Cell map[MAP_SIZE][MAP_SIZE]) {
+  for (int x = 0; x < MAP_SIZE; x++) {
+    for (int y = 0; y < MAP_SIZE; y++) {
+      Cell c;
+      c.type = EMPTY;
+      map[x][y] = c;
+    }
+  }
+}
+
 int main() {
+  Cell map[MAP_SIZE][MAP_SIZE];
+  init_map(map);
+
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bismillah");
 
   SetTargetFPS(60);
